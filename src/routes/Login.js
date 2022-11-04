a4c722a6839946ce040bff9a3e02aeb84bda86f2
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-04 18:38:26 +0900

    [feat] 로그인post

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,38 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Login_navbar from "../components/Login_navbar";
 import login_styles from "../static/css/login.module.css";
 import { useNavigate } from "react-router-dom";
-function Login() {
-    const navigate = useNavigate();
-    const move_to_signup = () => {
-        navigate("/Signup");
-    };
-    return (
-        <div className={login_styles.login_page}>
-            <Login_navbar />
-            <div className={login_styles.container}>
-                <div className={login_styles.input_container}>
-                    <div className={login_styles.one_input}>
-                        <label for="id" className={login_styles.label}>
-                            학번
-                        </label>
-                        <input type="text" id="id" className={login_styles.input}></input>
-                    </div>
+import axios from "axios";
 
-                    <div className={login_styles.one_input}>
-                        <label for="pw" className={login_styles.label}>
-                            비밀번호
-                        </label>
-                        <input type="text" id="pw" className={login_styles.input}></input>
-                    </div>
-                </div>
-                <button className={login_styles.login_button}>로그인</button>
-            </div>
-            <button className={login_styles.signup_button} onClick={move_to_signup}>
-                회원가입
-            </button>
+function Login() {
+  const navigate = useNavigate();
+  const move_to_signup = () => {
+    navigate("/Signup");
+  };
+
+  //   const [account, setAccount] = useState({
+  //     id: "",
+  //     pw: "",
+  //   });
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
+  //   useEffect(() => {
+  //     console.log(id, pw);
+  //   }, [id, pw]);
+
+  //   const onChangeAccount = (e) => {
+  //     setAccount({
+  //       ...account,
+  //       [e.target.name]: e.target.value,
+  //     });
+  //   };
+
+  const onChangeid = (e) => {
+    setId(e.target.value);
+  };
+  const onChangepw = (e) => {
+    setPw(e.target.value);
+  };
+  const post_acount = async () => {
+    const data = {
+      id: id,
+      pw: pw,
+    };
+    try {
+      const request = await axios
+        .post("http://127.0.0.1:8080/login", data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch {}
+  };
+  return (
+    <div className={login_styles.login_page}>
+      <Login_navbar />
+      <div className={login_styles.container}>
+        <div className={login_styles.input_container}>
+          <div className={login_styles.one_input}>
+            <label for="id" className={login_styles.label}>
+              학번
+            </label>
+            <input
+              type="text"
+              id="id"
+              name="id"
+              className={login_styles.input}
+              onChange={onChangeid}
+            ></input>
+          </div>
+
+          <div className={login_styles.one_input}>
+            <label for="pw" className={login_styles.label}>
+              비밀번호
+            </label>
+            <input
+              type="password"
+              id="pw"
+              name="pw"
+              className={login_styles.input}
+              onChange={onChangepw}
+            ></input>
+          </div>
         </div>
-    );
+        <button className={login_styles.login_button} onClick={post_acount}>
+          로그인
+        </button>
+      </div>
+      <button className={login_styles.signup_button} onClick={move_to_signup}>
+        회원가입
+      </button>
+    </div>
+  );
 }
 
 export default Login;
